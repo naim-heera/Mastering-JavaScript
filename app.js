261b3836549cf29233183b93cbc6989383321e24
Naim Heera <abunaimheera@gmail.com>
2022-06-13 15:03:42 +0300

    Typeconverion tested

--- app.js
+++ app.js
@@ -1,9 +1,113 @@
-let result = '';
-let i = 0;
+// let result = '';
+// let i = 0;
 
-do {
-    i = i + 1;
-    result = result + i;
-} while (i < 5);
+// do {
+//     i = i + 1;
+//     result = result + i;
+// } while (i < 5);
 
-console.log(result);
+// Typeconversion 
+
+let a = '1000';
+let b = 5;
+console.log(a);
+console.log(b);
+
+// Automatic typeconversion happening during multiplication 
+// console.log(a*b);
+
+// console.log(a+b);
+// console.log(typeof(Number(a)));
+// console.log(typeof(Number.parseInt(a)));
+// console.log(typeof(b.toString()));
+
+
+// Infinity can be converter to string but can't be converted to number
+
+// console.log(typeof(Number(Infinity))); // This is called Number constructor
+// console.log(typeof(Infinity.toString()));
+
+// Infinity can be also converted
+// console.log(Number(Infinity));
+// console.log(String(Infinity));
+// console.log(Boolean(Infinity));
+// console.log(Boolean(-Infinity));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
